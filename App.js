7b549c82bef72f1f6dfbf1377ae79a7da5aec989
Lainel John Dela Cruz <laineljohndelacruz@Lainels-MacBook-Pro.local>
2019-06-27 08:32:10 +0800

    enhances the app

--- App.js
+++ App.js
@@ -7,9 +7,9 @@ import ManageAdsPage from './app/pages/settings/settings.page'
 
 const MainNavigator = createStackNavigator({
   // Login: {screen: LoginPage, navigationOptions: {header: null}, key: 'login'}, 
-  Dashboard: {screen: DashboardPage, key:'dashboard', navigationOptions: {header: null}},
+  // Dashboard: {screen: DashboardPage, key:'dashboard', navigationOptions: {header: null}},
   // Settings: {screen: SettingsPage, key:'settings', navigationOptions: {header: null}},
-  // ManageAds: {screen: ManageAdsPage, key:'manageads', navigationOptions: {header: null}}
+  ManageAds: {screen: ManageAdsPage, key:'manageads', navigationOptions: {header: null}}
 });
 const App = createAppContainer(MainNavigator);
 export default App
