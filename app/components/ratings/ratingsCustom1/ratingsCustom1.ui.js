15ee700d65d786bef4b38b06d02d3013cb19d47c
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-07-03 11:39:56 +0800

    optimizes the scripting

--- app/components/ratings/ratingsCustom1/ratingsCustom1.ui.js
+++ app/components/ratings/ratingsCustom1/ratingsCustom1.ui.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {StyleSheet, View, Text } from 'react-native';
+import StarRating from 'react-native-star-rating';
 
 export class RatingsCustom1 extends Component {
     constructor(props){
@@ -9,13 +10,10 @@ export class RatingsCustom1 extends Component {
       return (
           <View>
               <View style={this.styles.container}>
-                <View style={this.styles.starReview}></View>
-                <View style={this.styles.starReview}></View>
-                <View style={this.styles.starReview}></View>
-                <View style={this.styles.starReview}></View>
-                <View style={this.styles.starReview}></View>
-                <Text style={this.styles.reviewText}>{this.props.review} of {this.props.reviewer}</Text>
-                
+                <View style={this.styles.starReview}>
+                    <StarRating disabled={false} maxStars={5} rating={this.props.review} />
+                </View>
+                <Text style={this.styles.reviewText}>{this.props.review} of {this.props.reviewer}</Text>                
               </View>
 
           </View>
@@ -28,10 +26,10 @@ export class RatingsCustom1 extends Component {
             flexDirection: 'row',
         },
         starReview:{
-            width:20,
+            // width:20,
             height:20,
-            backgroundColor:'yellow',
-            marginLeft: 1,
+            // backgroundColor:'yellow',
+            // marginLeft: 1,
         },
         reviewText:{
             marginLeft:5,
