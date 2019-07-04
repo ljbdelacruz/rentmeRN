d78ba2564bc44910dc0425a072df310d846f34d3
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-07-04 09:28:32 +0800

    enhancement on the dashboard and transfer all functionalities on the subpages

--- app/components/clientSide/subpage/dashboard/clientDashboard.sub.js
+++ app/components/clientSide/subpage/dashboard/clientDashboard.sub.js
@@ -3,37 +3,44 @@ import { StyleSheet, View, ScrollView, Button, Alert } from 'react-native';
 import {DashboardHeader1} from '../../../header/DashboardHeader1/dashboardHeader1.ui'
 import {RecommendedContainer1} from '../../../container/recommendedContainer1/recommendedContainer.ui'
 import {AdsList1} from '../../../container/ads/adsList1/adsList1.ui'
+import {getAds, getCategory, getRecommendedAds} from '../../../../services/api.service'
 
 export class ClientDashboardSubPage extends Component {
     constructor(prop){
         super(prop);
         this.state={
           ads:[],
+          adsFetchFinish:false,
           category:[],
-          recommendedAds:[]
+          recommendedAds:[],
         }
+        
     }
+    componentDidMount(){
+      this.fetchAds();
+    }
+
     render() {
       this.dest=this.props.ads;
       return (
             <View style={this.styles.container}>
                   <View style={this.styles.mainContainer}>
                       <ScrollView showsVerticalScrollIndicator={false}>
-                        <DashboardHeader1 categories={this.props.category} categoryOnClick={(category)=>{
-                          this.props.categoryOnClick(category);
+                        <DashboardHeader1 categories={this.state.category} categoryOnClick={(category)=>{
+                          this.selectCategory(category);
                         }} rightButtonPressed={()=>{
-                            this.props.rightButtonPressed();
+                          this.selectMenu();
                         }}></DashboardHeader1>
                         <View style={this.styles.adsContainer}>
-                          <AdsList1 aiHide={this.props.aiAds} adsList={this.props.ads} selectAds={(ads)=>{
-                            this.props.selectAds(ads);
+                          <AdsList1 aiHide={this.state.adsFetchFinish} adsList={this.state.ads} selectAds={(ads)=>{
+                            this.selectAds(ads);
                           }}
                           rightButtonOnClick={(userInfo)=>{
-                            this.props.rightButtonOnClick(userInfo);
+                            this.sendUserAMessage(userInfo);
                           }}></AdsList1>
                         </View>
-                        <RecommendedContainer1 ads={this.props.rads} onSelectAds={(ads)=>{
-                          this.props.onSelectAds(ads);
+                        <RecommendedContainer1 ads={this.state.recommendedAds} onSelectAds={(ads)=>{
+                            this.selectRecommendation(ads);
                         }}></RecommendedContainer1>
                       </ScrollView>
                   </View>
@@ -61,5 +68,56 @@ export class ClientDashboardSubPage extends Component {
           zIndex:2
         }
     });
+
+    //click handlers
+    selectCategory(category){
+      Alert.alert(category.name)
+    }
+    selectAds(ads){
+      this.navigatePages(2, {ads:ads});
+    }
+    selectRecommendation(ads){
+      this.navigatePages(2, {ads:ads});
+    }
+    selectMenu(){
+      this.navigatePages(1, {});
+    }
+    sendUserAMessage(userInfo){
+      Alert.alert(userInfo.title);
+    }
+    navigatePages(option, param){
+      const {navigate} = this.props.navigation;
+      switch(option){
+        case 1:
+          navigate('Settings');
+          break;
+        case 2:
+          navigate('ManageAds', param)
+          break;
+      }
+    }
+
+    //data fetching in this sub page
+    fetchData(){
+      getAds(function(data){
+        this.setState({ads:data});
+        this.setState({adsFetchFinish:true});
+      }.bind(this), function(err){
+        Alert.alert("Failed",JSON.stringify(err));
+        this.setState({adsFetchFinish:true});
+      }.bind(this))
+      getCategory(function(data){
+        this.setState({category:data});
+      }.bind(this), function(err){
+        Alert.alert("Failed",JSON.stringify(err));
+      }.bind(this))
+      getRecommendedAds(function(data){
+        this.setState({recommendedAds:data})
+      }.bind(this), function(err){
+        Alert.alert("Failed",JSON.stringify(err));
+      }.bind(this))
+    }
+
+
 
   }
--- app/pages/dashboard/dashboard.page.js
+++ app/pages/dashboard/dashboard.page.js
@@ -11,47 +11,29 @@ class DashboardPage extends React.Component {
     constructor(props){
       super(props)
       this.state={
-        ads:[],
-        adsFetchFinish:false,
-        category:[],
-        recommendedAds:[],
         type:1,
-
       }
-      this.ads=[]
-      this.fetchAds();
     }
     componentDidMount(){
-      this.fetchAds();
     }
 
     render() {        
         const {navigate} = this.props.navigation;
         if(this.state.type == 1){
-          return (<View></View>)
+          return (
+            <View>
+              <ClientDashboardSubPage></ClientDashboardSubPage>
+            </View>
+          )
+        }else{
+          return (
+            <View>
+              <HostDashboardSub></HostDashboardSub>
+            </View>
+          )
         }
 
-        return (
-          <View style={this.styles.container}>
-            {
 
-            }
-            <ClientDashboardSubPage aiAds={this.state.adsFetchFinish} ads={this.state.ads} category={this.state.category} rads={this.state.recommendedAds} categoryOnClick={(category)=>{
-              this.selectCategory(category);
-            }} rightButtonPressed={()=>{
-              this.selectMenu();
-            }} selectAds={(ads)=>{
-              this.selectAds(ads);
-            }} rightButtonOnClick={(userInfo)=>{
-              this.sendUserAMessage(userInfo);
-            }} onSelectAds={(ads)=>{
-              this.selectRecommendation(ads);
-            }}></ClientDashboardSubPage>
-
-
-            <HostDashboardSub></HostDashboardSub>
-          </View>
-        );
     }
     styles = StyleSheet.create({
         container:{
@@ -59,56 +41,9 @@ class DashboardPage extends React.Component {
             width:'100%',
             height:'100%',
         },
-
-
     });
-    selectCategory(category){
-      Alert.alert(category.name)
-    }
-    selectAds(ads){
-      this.navigatePages(2, {ads:ads});
-    }
-    selectRecommendation(ads){
-      this.navigatePages(2, {ads:ads});
-    }
-    selectMenu(){
-      this.navigatePages(1, {});
-    }
-    sendUserAMessage(userInfo){
-      Alert.alert(userInfo.title);
-    }
-    navigatePages(option, param){
-      const {navigate} = this.props.navigation;
-      switch(option){
-        case 1:
-          navigate('Settings');
-          break;
-        case 2:
-          navigate('ManageAds', param)
-          break;
-      }
-    }
-    fetchAds(){
-      getAds(function(data){
-        this.setState({ads:data});
-        this.setState({adsFetchFinish:true});
-      }.bind(this), function(err){
-        Alert.alert("Failed",JSON.stringify(err));
-        this.setState({adsFetchFinish:true});
-      }.bind(this))
-      getCategory(function(data){
-        this.setState({category:data});
-      }.bind(this), function(err){
-        Alert.alert("Failed",JSON.stringify(err));
-      }.bind(this))
-      getRecommendedAds(function(data){
-        this.setState({recommendedAds:data})
-      }.bind(this), function(err){
-        Alert.alert("Failed",JSON.stringify(err));
-      }.bind(this))
 
 
-    }
   }
 export default DashboardPage;
 
