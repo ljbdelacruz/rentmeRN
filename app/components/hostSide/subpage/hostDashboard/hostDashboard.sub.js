a89a8a30cb6d79692294c3a2f3a4902f5d201b92
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-07-05 10:06:22 +0800

    enhancements on hostdashboard and api services

--- app/components/hostSide/subpage/hostDashboard/hostDashboard.sub.js
+++ app/components/hostSide/subpage/hostDashboard/hostDashboard.sub.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, Alert } from 'react-native';
 import {CustomButton4} from '../../../buttons/customButton4/customButton4.ui'
-
+import {RatingsCustom1} from '../../../ratings/ratingsCustom1/ratingsCustom1.ui'
+import {getUserReview} from '../../../../services/api.service'
 export class HostDashboardSub extends Component {
     constructor(prop){
         super(prop);
         this.state={
-          buttons:[{title:'Manage Ads', value:3, param:{}}, {title:'Reviews', value:4, param:{}}, {title:'Create Ads', value:5, param:{}}]
+          buttons:[{title:'Manage Ads', value:3, param:{}}, {title:'Reviews', value:4, param:{}}, {title:'Create Ads', value:5, param:{}}],
+          review:{review:0, reviewer:0}
         }
+    }
+    componentDidMount(){
+      this.fetchData();
     }
     render() {
       return(
         <View>
             <View style={this.styles.headContainer}>
-              <View style={this.styles.headDashboard}></View>
+              <View style={this.styles.headDashboard}>
+                <View style={this.styles.ratingsContainer}>
+                  <RatingsCustom1 review={this.state.review.review} reviewer={this.state.review.reviewer}></RatingsCustom1>
+                </View>
+              </View>
             </View>
             <View style={this.styles.menuContainer}>
               {this.state.buttons.map(button => {
-                  return (
+                  return(
                     <View style={this.styles.btnContainer}>
                       <CustomButton4 mytitle={button.title} onClick={()=>{
                         this.props.navigatePages(button.value, button.param)
@@ -50,8 +59,23 @@ export class HostDashboardSub extends Component {
         width:'100%',
         backgroundColor:'#B0F4E6',
         zIndex:1,
-      },        
+        
+      },
+      ratingsContainer:{
+        alignContent:'center',
+        flex:1,
+        flexDirection:'column'
+      }
+
     });
 
+
+    fetchData(){
+      getUserReview(function(data){
+        this.setState({review:data});
+      }.bind(this), function(err){
+        Alert.alert("Failed", JSON.stringify(err));
+      }.bind(this))
+    }
 
   }
--- app/services/api.service.js
+++ app/services/api.service.js
@@ -4,6 +4,8 @@ axios.defaults.baseURL = 'http://192.168.88.23:8080';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
 
+
+//#region AdsRelated
 export async function getAds(success, err){
     return axios.get('/getAds').then(function(response){
         success(response.data);
@@ -18,6 +20,9 @@ export async function getRecommendedAds(success, err){
         err(error);
     });
 }
+//#endregion
+
+//#region Categories
 export async function getCategory(success, err){
     return axios.get('/getCategory').then(function(response){
         success(response.data);
@@ -25,3 +30,19 @@ export async function getCategory(success, err){
         err(error);
     });
 } 
+//#endregion
+
+//#region Reviews
+export async function getUserReview(success, err){
+    return axios.get('/userrating/getUserReview').then(function(response){
+        success(response.data);
+    }).catch(function (error){
+        err(error);
+    });
+} 
+
+//#endregion
+
+
+
+
