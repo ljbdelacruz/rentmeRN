c314c25229d8ca264389b84075f527a12b56bc13
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2019-07-11 16:46:49 +0800

    enhance the registration page

--- app/components/clientSide/subpage/registration/registration.sub.js
+++ app/components/clientSide/subpage/registration/registration.sub.js
@@ -3,16 +3,17 @@ import { StyleSheet, View, Alert } from 'react-native';
 import {CustomTF1} from '../../../textfields/customTF1/customTF1.ui'
 import {CustomButton1} from '../../../buttons/customButton1/customButton1.ui'
 import {CommonHeader1} from '../../../header/commonHeader1/commonHeader.ui'
+import {CustomTF2} from '../../../textfields/customTF2/customTF2.ui'
 
 export class ClientRegistrationSub extends Component {
     constructor(prop){
         super(prop);
         global.assetURL="http://192.168.88.23:8080/assets";
         this.state={
-          emailInput:{placeholder:'Email', value:''},
-          passwordInput:{placeholder:'Password'},
-          repasswordInput:{placeholder:'Retype Password'},
-          proceedButton:{label:'APPLY'},
+          emailInput:{placeholder:'Email', value:'', image:global.assetURL+"/images/username.png", type:"username"},
+          passwordInput:{placeholder:'Password', value:'', image:global.assetURL+"/images/password.png", type:"password"},
+          repasswordInput:{placeholder:'Retype Password', value:'', image:global.assetURL+"/images/password.png", type:"password"},
+          proceedButton:{label:'PROCEED'},
           backButton:{source:global.assetURL+"/images/l-arrow.png"}
       }
     }
@@ -27,29 +28,45 @@ export class ClientRegistrationSub extends Component {
                 </View>
                 <View style={this.styles.inputSub}>
                     <View style={this.styles.inputContainer}>
-                        <CustomTF1 placeholder={this.state.emailInput.placeholder} 
-                                onChangeText={(text)=>{
+                        <CustomTF2 placeholder={this.state.emailInput.placeholder} 
+                                   textContentType="username"
+                                   image={this.state.emailInput.image} 
+                                   onChangeText={(text)=>{
                                     //please do validation on the text before submitting it into the server
-                                    this.setState({emailInput:{placeholder:this.state.emailInput.placeholder, value:text}})
-                                }}></CustomTF1>
+                                    this.setState({emailInput:{placeholder:this.state.emailInput.placeholder, 
+                                                   value:text, 
+                                                   image:this.state.emailInput.image,
+                                                  type:this.state.emailInput.type}})
+                        }} />
                     </View>
                     
                     <View style={this.styles.inputContainer}>
-                        <CustomTF1 placeholder={this.state.passwordInput.placeholder}
-                                textContentType="password"
-                                secureTextEntry={true}
-                                onChangeText={(text) => {
-                                    this.setState({passwordInput:{placeholder:this.state.passwordInput.placeholder, value:text}})
-                                }}></CustomTF1>
+                        <CustomTF2 placeholder={this.state.passwordInput.placeholder} 
+                                   image={this.state.passwordInput.image} 
+                                   textContentType="password"
+                                   secureTextEntry={true}
+                                   onChangeText={(text)=>{
+                                    //please do validation on the text before submitting it into the server
+                                    this.setState({passwordInput:{placeholder:this.state.passwordInput.placeholder, 
+                                                   value:text, 
+                                                   image:this.state.passwordInput.image,
+                                                  type:this.state.passwordInput.type}})
+                        }} />
                     </View>
                     
                     <View style={this.styles.inputContainer}>
-                        <CustomTF1 placeholder={this.state.repasswordInput.placeholder}
-                                textContentType="password"
-                                secureTextEntry={true}
-                                onChangeText={(text) => {
-                                    this.setState({repasswordInput:{placeholder:this.state.repasswordInput.placeholder, value:text}})
-                                }}></CustomTF1>
+                        <CustomTF2 placeholder={this.state.repasswordInput.placeholder} 
+                                   secureTextEntry={true}
+                                   textContentType="password"
+                                   image={this.state.repasswordInput.image} 
+                                   mytype={this.state.repasswordInput.type}
+                                   onChangeText={(text)=>{
+                                    //please do validation on the text before submitting it into the server
+                                    this.setState({repasswordInput:{placeholder:this.state.repasswordInput.placeholder, 
+                                                   value:text, 
+                                                   image:this.state.repasswordInput.image,
+                                                  type:this.state.repasswordInput.type}})
+                        }} />
                     </View>
                     <View style={[this.styles.inputContainer, this.styles.buttonContainer]}>
                          <CustomButton1 title={this.state.proceedButton.label} onPress={()=>{Alert.alert("Process Pressed!")}}></CustomButton1>
@@ -60,10 +77,10 @@ export class ClientRegistrationSub extends Component {
         </View>
       );
     }
+
     styles = StyleSheet.create({
       headerContainer:{
         width:'100%',
-        // marginLeft:'5%',
       },
       container:{
         width:'100%',
@@ -77,7 +94,7 @@ export class ClientRegistrationSub extends Component {
           borderBottomRightRadius:10,
       },
       inputSub:{
-          marginTop:50
+          marginTop:30
       },
       inputContainer:{
           marginTop:10,
--- app/components/textfields/customTF2/customTF2.ui.js
+++ app/components/textfields/customTF2/customTF2.ui.js
@@ -3,51 +3,49 @@ import { Platform, TextInput, StyleSheet, Image, View } from 'react-native';
 export class CustomTF2 extends Component {
     constructor(prop){
         super(prop);
-        this.viewModel={text:'', placeholder:'', imageSource:'./facebook.png'}
         console.log(this.props.placeholder);
     }
     render(){
       return (
-         <View style={styles.usernameTf}>
-            <View style={styles.rect} />
-            <Image
-              style={styles.image}
-              source={require(this.viewModel.imageSource)}
-            />
-            <TextInput
-              style={styles.textInput}
-              placeholder="Username"
-              placeholderTextColor="rgba(0,0,0,0.1)"
-            />
+         <View style={this.styles.usernameTf}>
+            <View style={this.styles.rect} >
+              <Image style={this.styles.image}
+                     source={{uri:this.props.image}}/>
+              <TextInput
+                {...this.props}
+                style={this.styles.textInput}
+                placeholder={this.props.placeholder}
+                placeholderTextColor="rgba(0,0,0,0.1)"
+                onChangeText={(text)=>{
+                  if(this.props.onChangeText){
+                      this.props.onChangeText(text);
+                  }            
+                }}/>
+            </View>
           </View>
       );
     }
     styles = StyleSheet.create({
-        usernameTf: {
-            height: 43,
+          usernameTf: {
+              height: 49,
+              width: '100%',
+          },
+          rect: {
             width: '100%',
-        },
-        rect: {
-            left: "0%",
-            width: '100%',
-            height: 44,
-            borderWidth: 1,
-            borderColor: "rgba(123,123,123,1)",
-            borderRadius: 20
+            height:'100%',
+            borderRadius: 20,
+            flexDirection:'row',
+            backgroundColor:'white',
           },
           image: {
-            top: 14,
-            left: 12,
-            width: 16,
-            height: 16,
-            position: "absolute"
+            marginTop:'4%',
+            marginLeft:10,
+            width:'50%',height:'50%',
+            flex:0.1,
           },
           textInput: {
-            top: 14,
-            left: "10.91%",
-            width: '90%',
-            height: 16,
-            position: "absolute"
+            flex:1,
+            color:'grey',
           },
 
     });
